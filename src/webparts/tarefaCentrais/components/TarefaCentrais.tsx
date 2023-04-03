c962480a7e8cdc0ee483abcd5b51345ed3ba0d57
SilasReis <s-belau@live.com>
2023-04-03 12:01:08 -0300

    inserção web part tarefaCentrais

---
+++ src/webparts/tarefaCentrais/components/TarefaCentrais.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+// import styles from './TarefaCentrais.module.scss';
+import { ITarefaCentraisProps } from './ITarefaCentraisProps';
+import Header from "../../colabEmissor/components/ColabHeader";
+import { sp } from "@pnp/sp/presets/all";
+import "@pnp/sp/lists";
+import "@pnp/sp/items";
+import '../../../style/index.css';
+import customStyle from '../../../style/colab.module.scss';
+import TarefaSistemicos from "../../tarefaCentrais/components/TarefaSistemicos";
+
+export default class TarefaCentrais extends React.Component<ITarefaCentraisProps, {}> {
+  public render(): React.ReactElement<ITarefaCentraisProps> {
+    const {
+    } = this.props;
+    const webUrl = window.location.protocol + "//" + window.location.hostname + "/" + window.location.pathname.split('/')[1] + "/" + window.location.pathname.split('/')[2]
+    sp.setup({ sp: { headers: { Accept: "application/json;odata=verbose", }, baseUrl: webUrl }, });
+
+    return (
+      <>
+      <section className={customStyle.colabNormativos}>
+        <div className={`${customStyle['bg-white']} ${customStyle['border']} ${customStyle['rounded-5']}`}>
+          <div className={customStyle.container} style={{ paddingBottom: '50px' }}>
+            <Header idNormativo='3543' />  
+            <TarefaSistemicos idNormativo='3543'></TarefaSistemicos>
+          </div>
+        </div>
+      </section>
+    </>
+    );
+  }
+}
---
+++ src/webparts/tarefaCentrais/components/TarefaSistemicos.tsx
@@ -0,0 +1,84 @@
+import * as React from 'react';
+import { useEffect, useState } from 'react';
+import { sp } from "@pnp/sp";
+import "@pnp/sp/webs";
+import "@pnp/sp/site-users/web";
+import "@pnp/sp/lists";
+import "@pnp/sp/items";
+import "@pnp/sp/attachments";
+import "@pnp/sp/files";
+import "@pnp/sp/folders";
+import { ITarefaSistemicorProps } from './ITarefaSistemicos.Props';
+
+export interface ICentraisProps {
+    Title: string;
+    CodigoCentral: string
+}
+
+export default function TarefaSitemicos(props: ITarefaSistemicorProps): JSX.Element {
+    const [centrais, setCentrais] = useState<ICentraisProps[]>([]);
+    
+
+    useEffect(() => {
+        const webUrl = window.location.protocol + "//" + window.location.hostname + "/" + window.location.pathname.split('/')[1] + "/" + window.location.pathname.split('/')[2]
+        sp.setup({
+            sp: {
+                headers: {
+                    Accept: "application/json;odata=verbose",
+                },
+                baseUrl: webUrl
+            },
+        });
+
+        sp.web.lists.getByTitle('Centrais').items.select('*,Title,CodigoCentral')()
+            .then((data: ICentraisProps[]) => {
+                setCentrais(data)
+                console.log(data);
+            });
+    }, []);
+    return (
+        <>
+            <div className="row border-top" style={{ paddingTop: '1rem' }}>
+                <div className="col-md-4">
+                    <h4>Participações Etapa Colaborativa</h4>
+                </div>
+            </div>
+            <div style={{ marginTop: 20 }}>
+                <table className="table table-group-divide">
+                    <thead className="table-light">
+                        <tr>
+                            <th><input type="checkbox"></input></th>
+                            <th>Código</th>
+                            <th>Nome</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {centrais.map((central, idx) => {
+                            return (
+                                <>
+                                    <tr>
+                                        <td><input type="checkbox"></input></td>
+                                        <td>{central.CodigoCentral}</td>
+                                        <td>{central.Title}</td>
+                                    </tr>
+                                </>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
+            <div style={{ marginTop: 30 }}>
+                <h6>Selecione uma data para o período de colaboração:</h6>
+                <div className="col-5">
+                    <input type="date" className="form-control" id="date" />
+                </div>
+            </div>
+            <div style={{ marginTop: 40 }}>
+                <div className='col'>
+                    <button className='btn btn-success'>Enviar Tarefa</button>
+                </div>
+            </div>
+
+        </>
+    );
+}
