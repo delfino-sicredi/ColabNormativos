e373e19cadace90fe02f44c1aee32f81ed8a528a
SilasReis <s-belau@live.com>
2023-04-10 17:14:30 -0300

    Criando tela para singulares

--- src/webparts/tarefaSingulares/components/TarefaSingulares.tsx
+++ src/webparts/tarefaSingulares/components/TarefaSingulares.tsx
@@ -1,43 +1,26 @@
 import * as React from 'react';
-import styles from './TarefaSingulares.module.scss';
+import customStyle from '../../../style/colab.module.scss';
 import { ITarefaSingularesProps } from './ITarefaSingularesProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import { sp } from "@pnp/sp/presets/all";
+import Header from '../../../components/ColabHeader';
 
 export default class TarefaSingulares extends React.Component<ITarefaSingularesProps, {}> {
   public render(): React.ReactElement<ITarefaSingularesProps> {
     const {
-      description,
-      isDarkTheme,
-      environmentMessage,
-      hasTeamsContext,
-      userDisplayName
     } = this.props;
+    const webUrl = window.location.protocol + "//" + window.location.hostname + "/" + window.location.pathname.split('/')[1] + "/" + window.location.pathname.split('/')[2]
+    sp.setup({ sp: { headers: { Accept: "application/json;odata=verbose", }, baseUrl: webUrl }, });
 
     return (
-      <section className={`${styles.tarefaSingulares} ${hasTeamsContext ? styles.teams : ''}`}>
-        <div className={styles.welcome}>
-          <img alt="" src={isDarkTheme ? require('../assets/welcome-dark.png') : require('../assets/welcome-light.png')} className={styles.welcomeImage} />
-          <h2>Well done, {escape(userDisplayName)}!</h2>
-          <div>{environmentMessage}</div>
-          <div>Web part property value: <strong>{escape(description)}</strong></div>
-        </div>
-        <div>
-          <h3>Welcome to SharePoint Framework!</h3>
-          <p>
-            The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It&#39;s the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
-          </p>
-          <h4>Learn more about SPFx development:</h4>
-          <ul className={styles.links}>
-            <li><a href="https://aka.ms/spfx" target="_blank" rel="noreferrer">SharePoint Framework Overview</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank" rel="noreferrer">Use Microsoft Graph in your solution</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank" rel="noreferrer">Build for Microsoft Teams using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank" rel="noreferrer">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank" rel="noreferrer">Publish SharePoint Framework applications to the marketplace</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank" rel="noreferrer">SharePoint Framework API reference</a></li>
-            <li><a href="https://aka.ms/m365pnp" target="_blank" rel="noreferrer">Microsoft 365 Developer Community</a></li>
-          </ul>
+      <>
+      <section className={customStyle.colabNormativos}>
+        <div className={`${customStyle['bg-white']} ${customStyle['border']} ${customStyle['rounded-5']}`}>
+          <div className={customStyle.container} style={{ paddingBottom: '50px' }}>
+            <Header idNormativo='3543' /> 
+          </div>
         </div>
       </section>
+    </>
     );
   }
 }
